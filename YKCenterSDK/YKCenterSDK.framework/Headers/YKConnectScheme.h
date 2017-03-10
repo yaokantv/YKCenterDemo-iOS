5f9166504634bd4c56d90f77295df716328d5010
Don <donly@magicalboy.com>
2017-03-10 16:31:59 +0800

    update sdk

---
+++ YKCenterSDK/YKCenterSDK.framework/Headers/YKConnectScheme.h
@@ -0,0 +1,13 @@
+//
+//  YKConnectScheme.h
+//  YKCenterSDK
+//
+//  Created by Don on 2017/1/12.
+//  Copyright © 2017年 Shenzhen Yaokan Technology Co., Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YKConnectScheme : NSObject
+
+@end
