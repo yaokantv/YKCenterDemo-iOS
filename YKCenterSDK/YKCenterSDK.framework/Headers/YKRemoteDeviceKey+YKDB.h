46bd8fdf363726797246f4620316e65036a5fbd9
Don <donly@magicalboy.com>
2017-06-30 10:33:09 +0800

    demp app v1.5 YKCenterSDK v1.1.1024 more detail see change_log wiki

---
+++ YKCenterSDK/YKCenterSDK.framework/Headers/YKRemoteDeviceKey+YKDB.h
@@ -0,0 +1,42 @@
+//
+//  YKRemoteDeviceKey+YKDB.h
+//  YKCenterSDK
+//
+//  Created by Don on 2017/6/29.
+//  Copyright © 2017年 Shenzhen Yaokan Technology Co., Ltd. All rights reserved.
+//
+
+#import <YKCenterSDK/YKCenterSDK.h>
+
+static NSString *KeyPowerOn = @"on";
+static NSString *KeyPowerOff = @"off";
+
+static NSString *ACModelAuto   = @"a";    // 自动
+static NSString *ACModelCool   = @"r";    // 制冷
+static NSString *ACModelDry    = @"d";    // 抽湿
+static NSString *ACModelWind   = @"w";    // 送风
+static NSString *ACModelHot    = @"h";    // 制热
+
+
+@interface YKRemoteDeviceKey (YKDB)
+
++ (YKRemoteDeviceKey *)remoteDeviceKeyIn:(YKRemoteDevice *)remoteDevice
+                                withMode:(NSString *)mode
+                                    temp:(NSUInteger)temp
+                                   speed:(NSUInteger)speed
+                                   windU:(NSUInteger)windU
+                                   windL:(NSUInteger)windL
+                           asteriskDatas:(NSArray *)asteriskDatas;
+
++ (YKRemoteDeviceKey *)remoteDeviceKeyInRemoteDevice:(YKRemoteDevice *)remote
+                                                 key:(NSString *)key;
+
++ (NSArray<YKRemoteDeviceKey *> *)asteriskKeysWithRemote:(YKRemoteDevice *)remote;
+
++ (YKRemoteDeviceKey *)remoteDeviceKeyWith:(NSArray<YKRemoteDeviceKey *> *)asteriskKeys
+                                upDownWind:(NSUInteger)wind;
+
++ (YKRemoteDeviceKey *)remoteDeviceKeyWith:(NSArray<YKRemoteDeviceKey *> *)asteriskKeys
+                             leftRightWind:(NSUInteger)wind;
+
+@end
